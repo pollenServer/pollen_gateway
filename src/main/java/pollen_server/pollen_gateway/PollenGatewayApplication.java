ead0c4bc3d0d5b97001ecb0d2087da52142315fe
wooseobee <dntjq0807@naver.com>
2023-05-22 18:14:38 +0900

    init pollen_gateway

---
+++ src/main/java/pollen_server/pollen_gateway/PollenGatewayApplication.java
@@ -0,0 +1,13 @@
+package pollen_server.pollen_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollenGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollenGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pollen_server/pollen_gateway/PollenGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package pollen_server.pollen_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollenGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
